b839973e28620aab249dd539c2ba6eed6126db77
Rafael Garrote Hernández <rafaelgarrote@MacBook-Pro-de-Rafael.local>
2019-01-13 20:36:36 +0100

    prepared for class

--- src/main/scala/com/datahack/bootcamp/spark/exercises/twitter/utils/FileUtils.scala
+++
@@ -1,9 +0,0 @@
-package com.datahack.bootcamp.spark.exercises.twitter.utils
-
-import scala.io.Source
-
-object FileUtils {
-
-  def parseEntitiesFile(url: String): List[String] = Source.fromFile(url).getLines.toList
-
-}
