edbbb85ff05a8052be239ebad5eb792765088795
Rafael Garrote Hernández <rafaelgarrote@MacBook-Pro-de-Rafael.local>
2019-01-17 20:43:14 +0100

    tweeter

--- src/main/scala/com/datahack/bootcamp/spark/streaming/TrendingTopic.scala
+++ src/main/scala/com/datahack/bootcamp/spark/streaming/TrendingTopic.scala
@@ -39,67 +39,22 @@ object TrendingTopic extends App {
   // trendingTopic(ssc)
 
   // Número de tweets disponibles cada 10 segundos
-  def countTweetsEach20Seconds(ssc: StreamingContext): Unit = {
-    val stream: ReceiverInputDStream[Status] = TwitterUtils.createStream(ssc, twitterAuth = Some(auth))
-    stream.count().print()
-
-    ssc.start()
-    ssc.awaitTermination()
-  }
+  def countTweetsEach20Seconds(ssc: StreamingContext): Unit = ???
 
   // Número de tweets en inglés (en) cada 10 segundos
-  def countEnglishTweetsEach20Seconds(ssc: StreamingContext): Unit = {
-    val stream: ReceiverInputDStream[Status] = TwitterUtils.createStream(ssc, twitterAuth = Some(auth))
-    stream.filter(tweet => tweet.getLang.equals("en")).count().print()
-
-    ssc.start()
-    ssc.awaitTermination()
-  }
+  def countEnglishTweetsEach20Seconds(ssc: StreamingContext): Unit = ???
 
   // Número de tweets en inglés en el último minuto cada 20 secundos
-  def countEnglishTweetsLastMinute(ssc: StreamingContext): Unit = {
-    val stream: ReceiverInputDStream[Status] = TwitterUtils.createStream(ssc, twitterAuth = Some(auth))
-    stream.filter(tweet => tweet.getLang.equals("en")).countByWindow(Minutes(1), Seconds(20)).print()
+  def countEnglishTweetsLastMinute(ssc: StreamingContext): Unit = ???
 
-    ssc.start()
-    ssc.awaitTermination()
-  }
   // Número de tweets por idioma en el último minuto
-  def countTweetsByLangLastMinute(ssc: StreamingContext): Unit = {
-    val stream: ReceiverInputDStream[Status] = TwitterUtils.createStream(ssc, twitterAuth = Some(auth))
-    stream.map(tweet => (tweet.getLang, 1)).reduceByKeyAndWindow((x, y) => x + y, Minutes(1)).print()
+  def countTweetsByLangLastMinute(ssc: StreamingContext): Unit = ???
 
-    ssc.start()
-    ssc.awaitTermination()
-  }
   // Hashtag y su número de ocurrencias para los tweets en inglés con más de una ocurrencia en los últimos 60 segundos
   // Ayuda: utiliza el método transform
-  def englishHashTagsWithMoreThanOneOccurrences(ssc: StreamingContext) {
-    val stream: ReceiverInputDStream[Status] = TwitterUtils.createStream(ssc, twitterAuth = Some(auth))
-    stream.filter(tweet => tweet.getLang.equals("en"))
-      .flatMap(tweet => tweet.getHashtagEntities)
-      .map(hashTag => (hashTag.getText, 1)).reduceByKeyAndWindow((x, y) => x + y, Minutes(1))
-      .transform(rdd => rdd.filter(_._2 > 1)).print()
-
-    ssc.start()
-    ssc.awaitTermination()
-  }
+  def englishHashTagsWithMoreThanOneOccurrences(ssc: StreamingContext): Unit = ???
 
   // Los diez hashtags más populares del último minuto para los tweets en inglés
   // Ayuda: utiliza el método transform y forEachRDD
-  def trendingTopic(ssc: StreamingContext) {
-    val stream: ReceiverInputDStream[Status] = TwitterUtils.createStream(ssc, twitterAuth = Some(auth))
-    stream.filter(tweet => tweet.getLang.equals("en"))
-      .flatMap(tweet => tweet.getHashtagEntities)
-      .map(hashTag => (hashTag.getText, 1)).reduceByKeyAndWindow((x, y) => x + y, Minutes(1))
-      .transform(rdd => rdd.sortBy(hashtagPair => hashtagPair._2, false))
-      .foreachRDD(rdd => {
-        val topList = rdd.take(10)
-        println("\nPopular topics in last 60 seconds (%s total):".format(rdd.count()))
-        topList.foreach { case (tag, count) => println("%s (%d tweets)".format(tag, count)) }
-      })
-
-    ssc.start()
-    ssc.awaitTermination()
-  }
+  def trendingTopic(ssc: StreamingContext): Unit = ???
 }
